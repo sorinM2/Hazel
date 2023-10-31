6fa4ae65a0dddff1eb9d28b72fe8c287262adbfd
sorinM2 <sorinmarian1199@gmail.com>
2023-10-31 20:37:36 +0200

    ba

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+//-----Entry point------------------
+#include "Hazel/EnteryPoint.h"
+//---------------------------------
+
+#include <stdio.h>
---
+++ Hazel/src/Hazel/EnteryPoint.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Hazel.h>
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	Hazel::Application* app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
